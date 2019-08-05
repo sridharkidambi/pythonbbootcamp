431050b5351a5ee0abd80efb850ce74960f0a271
SridharKidambi <sridharhere@gmail.com>
2019-08-05 11:18:23 +0530

    basic commands

--- datatypes.py
+++ datatypes.py
@@ -1,1 +1,21 @@
-print(' hello World')
+max_num = float("-inf") 
+print(' hello \n World')
+print(len(' hello \n World'))
+print(2**4 )
+
+sk=2
+print(sk)
+print(type(sk))
+sk=['me','da']
+print(sk)
+print(sk[0])
+print(type(sk))
+mystring="Sridhar Kidambi"
+print(mystring)
+print(mystring[::])
+print(mystring[::2])
+print(mystring[2::])
+print(mystring[2:6:2])
+print(mystring[2:])
+print(mystring[:3])
+print(mystring[2:10])
