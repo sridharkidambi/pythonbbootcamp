e7ffe80e05d8f90cc1c1f52a3c76b23af86d8356
SridharKidambi <sridharhere@gmail.com>
2019-09-03 12:57:26 +0530

    packages

--- pkgMod/program.py
+++ pkgMod/program.py
@@ -4,3 +4,11 @@ from mainpkg.subpkg import subfile
 
 myfuncprogram('Sridhar Kidambi')
 subfile.subpack()
+
+def progfunc():
+    print('i am being run now on being called.')
+
+if __name__ =='__main__':
+    print('this is run directly')
+else:
+    print(' iam being called by someone')
---
+++ pkgMod/two.py
@@ -0,0 +1,9 @@
+import program
+
+print (' i am 2.py')
+program.progfunc()
+
+if __name__ =='__main__':
+    print('i am 2.py')
+else:
+    print(' iam being called by someone')
