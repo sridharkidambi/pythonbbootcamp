a7183a88804e5408eede8b472717a3195d97ec2e
SridharKidambi <sridharhere@gmail.com>
2019-09-05 13:00:00 +0530

    code changes

--- jsonparsing.py
+++ jsonparsing.py
@@ -1,9 +1,16 @@
 import json
+from collections import namedtuple
+import namedtupled
 
 file_obj=open('./sample.json','r')
 
 json_object=  json.load(file_obj)
 
 
-print(json_object['accounting'][0])
-print(type(json_object['accounting']))
+# print(json_object['accounting'][0])
+# print(type(json_object['accounting']))
+
+namedTupleConstructor = namedtuple('myNamedTuple', ' '.join(sorted(json_object.keys())))
+nt= namedTupleConstructor(**json_object)
+cat = namedtupled.map(json_object)
+print(cat.accounting[0].lastName)
